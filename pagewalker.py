c45262a28b92fb0280bc2274a41771bc3e0af48e
Willenst <willenst@gmail.com>
2024-11-13 14:24:25 +0200

    fix

--- pagewalker.py
+++ pagewalker.py
@@ -4,7 +4,7 @@ class Page:
     cr3_register = None
 
     def __init__(self, virtual_address):
-        self.virtual = virtual_address
+        self.virtual = int(virtual_address,16)
         self.indexes = None
         self.pgd = None
         self.pud = None
@@ -24,16 +24,16 @@ class Page:
         addr = addr >> 12
         return [((addr >> (i * 9)) & 0x1FF) * 8 for i in range(4)][::-1]
 
-    @staticmethod
-    def huge_1gb(address):
+
+    def huge_1gb(self, address):
         addr = address & ~((1 << 30) - 1) & ((1 << 51) - 1)
-        addr = addr + address & ((1 << 30) - 1)
+        addr = addr + self.virtual & ((1 << 30) - 1)
         return addr
 
-    @staticmethod
-    def huge_2mb(address):
+
+    def huge_2mb(self, address):
         addr = address & ~((1 << 21) - 1) & ((1 << 51) - 1)
-        addr = addr + address & ((1 << 21) - 1)
+        addr = addr + self.virtual & ((1 << 21) - 1)
         return addr
 
     def get_phys_address(self, address):
@@ -51,8 +51,7 @@ class Page:
         return int(value_str, 16)
 
     def pgd_scan(self, addr):
-        address = int(addr,16)
-        self.indexes = Page.get_indexes(address)
+        self.indexes = Page.get_indexes(self.virtual)
         pgd_start = Page.cr3_register
         self.pgd = pgd_start + self.indexes[0]
 
@@ -61,18 +60,28 @@ class Page:
 
         pmd_start = Page.get_phys_address(self, self.pud)
         if self.huge:
-            self.phys = Page.huge_1gb(pmd_start)
+            self.phys = Page.huge_1gb(self, pmd_start)
             return
         self.pmd = pmd_start + self.indexes[2]
 
         pt_start = Page.get_phys_address(self, self.pmd)
         if self.huge:
-            self.phys = Page.huge_2mb(pt_start)
+            self.phys = Page.huge_2mb(self, pt_start)
             return
         self.pt = pt_start + self.indexes[3]
 
         phys_start = Page.get_phys_address(self, self.pt)
         self.phys = phys_start & ~((1 << 12) - 1) & ((1 << 63) - 1)
+
+def format_output(addr1, addr2, addr3 ,addr4, addr5):
+    address_list = [addr1, addr2, addr3 ,addr4, addr5]
+    return_list = []
+    for addr in address_list:
+        try:
+            return_list.append(hex(addr))
+        except:
+            return_list.append('N/A')
+    return return_list
 
 def pgd_scan(address_str):
     page = Page(address_str)
@@ -83,7 +92,8 @@ def pgd_scan(address_str):
     print('-'*(15*5+20))
     print(f"{'index:':<20}|{page.indexes[0]//8:<15}|{page.indexes[1]//8:<15}|{page.indexes[2]//8:<15}|{page.indexes[3]//8:<15}|{page.huge:<15}")
     print('-'*(15*5+20))
-    print(f"{'address:':<20}|{hex(page.pgd):<15}|{hex(page.pud):<15}|{hex(page.pmd):<15}|{hex(page.pt):<15}|{hex(page.phys):<15}")
+    addresses = format_output(page.pgd, page.pud, page.pmd, page.pt, page.phys)
+    print(f"{'address:':<20}|{addresses[0]:<15}|{addresses[1]:<15}|{addresses[2]:<15}|{addresses[3]:<15}|{addresses[4]:<15}")
     print()
 
 def pgd_phys_search(range_start, range_end, range_step, phys_address):
